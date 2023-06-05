eff01fbfc7be112e62ea0f4a72b78a458dc71d4d
Jason <jasonvan84@gmail.com>
2023-06-05 11:29:17 -0400

    REST API

--- src/main/java/com/skillstorm/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/skillstorm/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.skillstorm.rest.webservices.restfulwebservices;
+package com.skillstorm.rest.webservices.restfulwebservices.helloworld;
 
 public class HelloWorldBean {
 
--- src/main/java/com/skillstorm/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/skillstorm/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,10 @@
-package com.skillstorm.rest.webservices.restfulwebservices;
+package com.skillstorm.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
